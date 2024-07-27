6cbdfbaae589aacce0591c95f3c510f138b4b8c3
daniela carvajal <145785229+carvajaldaniela@users.noreply.github.com>
2024-07-27 10:45:44 -0600

    UpdAt.php

--- HTML/UpdAt.php
+++ HTML/UpdAt.php
@@ -1,79 +1,67 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agregar Atleta</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <link href="../CSS/insAt.css" rel="stylesheet">
+    <title>Asociación Esparzana de Tenis de Mesa</title>
+    <link rel="stylesheet" href="../CSS/admin.css">
+    <script
+      src="https://kit.fontawesome.com/a82054a465.js"
+      crossorigin="anonymous"
+    ></script>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
-    <div class="container mt-3">
-        <img src="../img/headerimage.png" alt="Encabezado" class="header-image">
-        <h1 class="mt-3 mb-4">Actualizar Atleta</h1>
-       
-  
-        <form action="../PHP/modif.php" method="POST">
-            <div class="mb-3">
-                <label for="deportistas" class="form-label">Seleccionar Deportista:</label>
-                <select id="deportista" name="userId">
-                 
-                 <?php include '../PHP/filtrar.php'; ?>
-                  
-                </select>
-            </div>
-            <div class="row">
-                <div class="col-md-6 mb-3">
-                    <label for="userId" class="form-label">Cedula:</label>
-                    <input type="text" class="form-control" id="userId" name="userId" required>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <label for="name_user" class="form-label">Nombre:</label>
-                    <input type="text" class="form-control" id="name_user" name="name_user" required>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <label for="apellidoPat" class="form-label">Apellido Paterno:</label>
-                    <input type="text" class="form-control" id="apellidoPat" name="apellidoPat" required>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <label for="apellidoMar" class="form-label">Apellido Materno:</label>
-                    <input type="text" class="form-control" id="apellidoMar" name="apellidoMar" required>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <label for="tipoSangre" class="form-label">Tipo Sangre:</label>
-                    <input type="text" class="form-control" id="tipoSangre" name="tipoSangre" required>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <label for="lateralidad" class="form-label">Lateralidad:</label>
-                    <input type="text" class="form-control" id="lateralidad" name="lateralidad" required>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <label for="enfermedades" class="form-label">Enfermedades:</label>
-                    <input type="text" class="form-control" id="enfermedades" name="enfermedades" required>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <label for="sexo" class="form-label">Sexo:</label>
-                    <input type="text" class="form-control" id="sexo" name="sexo" required>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <label for="edad" class="form-label">Edad:</label>
-                    <input type="text" class="form-control" id="edad" name="edad" required>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <label for="peso" class="form-label">Peso:</label>
-                    <input type="text" class="form-control" id="peso" name="peso" required>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <label for="altura" class="form-label">Altura:</label>
-                    <input type="text" class="form-control" id="altura" name="altura" required>
-                </div>
-            </div>
-            <button type="submit" class="btn btn-primary">Agregar Atleta</button>
-           
-        </form>
+    <header>
+        <div class="container-fluid header-container"></div>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Anton&display=swap">                
+                  <span>ASOCIACIÓN ESPARZANA</span>
+                  <img src="../img/pelotaicon.png" alt="Logo" width="100" height="100" />
+                  <span>DE TENIS DE MESA</span>
+        </div>
+    </header>
+    <div class="main-content">
+        <nav id="sidebarMenu">
+            <ul>
+                <li><a href="#">Jugadores</a></li>
+                <li><a href="#">Evaluación física</a></li>
+                <li><a href="InsAt.html">Agregar</a></li>
+                <li><a href="UpdAt.php">Editar</a></li>
+                <li><a href="#">Borrar</a></li>
+                <li><a href="#">Atletas destacados</a></li>
+            </ul>
+        </nav>
+        <main>
+            <section class="profile-search">
+                <h2>Perfiles</h2>
+                <input type="text" placeholder="Buscar">
+            </section>
+            <section class="profiles">
+                <!-- Perfiles de los jugadores -->
+            </section>
+        </main>
     </div>
-  
+    <footer class="footer">
+      <div class="box">
+        <h4>Síguenos</h4>
+        <div class="social-net">
+          <a
+            href="https://www.facebook.com/aeteme.esparza.9"
+            class="fa fa-facebook"
+          ></a>
+          <a
+            href="https://www.instagram.com/aetemecr?igsh=Z2hvbWkxMHJpd3B4"
+            class="fa fa-instagram"
+          ></a>
+          <a href="https://wa.me/50686596969" class="fa fa-whatsapp"></a>
+        </div>
+      </div>
+      <div class="group-2">
+        <small>&copy; 2024 <b>AETEME</b> - All rights reserved</small>
+      </div>
+    </footer>
   
 </body>
 </html>
+
