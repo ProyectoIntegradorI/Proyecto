ef23a511ceaeda9ec9b689d333eabd93b9895b5f
daniela carvajal <145785229+carvajaldaniela@users.noreply.github.com>
2024-07-27 11:04:59 -0600

    admin.php

--- HTML/admin.php
+++ HTML/admin.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Asociación Esparzana de Tenis de Mesa</title>
-    <link rel="stylesheet" href="/CSS/admin.css">
+    <link rel="stylesheet" href="../CSS/admin.css">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a82054a465.js" crossorigin="anonymous"></script>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-
 </head>
 <body>
     <header>
-        <div class="container-fluid header-container"></div>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Anton&display=swap">                
-                  <span>ASOCIACIÓN ESPARZANA</span>
-                  <img src="/img/pelotaicon.png" alt="Logo" width="100" height="100" />
-                  <span>DE TENIS DE MESA</span>
+        <div class="container-fluid header-container">
+            <span>ASOCIACIÓN ESPARZANA</span>
+            <img src="../img/pelotaicon.png" alt="Logo" width="100" height="100" />
+            <span>DE TENIS DE MESA</span>
         </div>
     </header>
     <div class="main-content">
@@ -22,8 +22,8 @@
             <ul>
                 <li><a href="#">Jugadores</a></li>
                 <li><a href="#">Evaluación física</a></li>
-                <li><a href="#">Agregar</a></li>
-                <li><a href="#">Editar</a></li>
+                <li><a href="InsAt.html">Agregar</a></li>
+                <li><a href="UpdAt.php">Editar</a></li>
                 <li><a href="#">Borrar</a></li>
                 <li><a href="#">Atletas destacados</a></li>
             </ul>
@@ -40,23 +40,16 @@
     </div>
     <footer class="footer">
         <div class="box">
-          <h4>Síguenos</h4>
-          <div class="social-net">
-            <a
-              href="https://www.facebook.com/aeteme.esparza.9"
-              class="fa fa-facebook"
-            ></a>
-            <a
-              href="https://www.instagram.com/aetemecr?igsh=Z2hvbWkxMHJpd3B4"
-              class="fa fa-instagram"
-            ></a>
-            <a href="https://wa.me/50686596969" class="fa fa-whatsapp"></a>
-          </div>
+            <h4>Síguenos</h4>
+            <div class="social-net">
+                <a href="https://www.facebook.com/aeteme.esparza.9" class="fa fa-facebook"></a>
+                <a href="https://www.instagram.com/aetemecr?igsh=Z2hvbWkxMHJpd3B4" class="fa fa-instagram"></a>
+                <a href="https://wa.me/50686596969" class="fa fa-whatsapp"></a>
+            </div>
         </div>
         <div class="group-2">
-          <small>&copy; 2024 <b>AETEME</b> - All rights reserved</small>
+            <small>&copy; 2024 <b>AETEME</b> - All rights reserved</small>
         </div>
-      </footer>
-  
+    </footer>
 </body>
 </html>
